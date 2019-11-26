1f82693eada1e06bebd65b2aa75f445a59dedc83
Shuo Kong on iMac <skong.astro@gmail.com>
2019-11-26 16:12:46 -0700

    show Bally BGPS regions in white

--- showpeak.py
+++ showpeak.py
@@ -34,7 +34,7 @@ if SEpeak == 1:
     mincolor = 1.8
     ff.show_colorscale(cmap='gist_heat', vmin=mincolor, vmax=maxcolor, stretch='linear')
     ff.show_contour(data=sebgps, levels=np.arange(0.4,5.,0.5), colors='white', linewidths=0.5) 
-    ff.show_regions('rebull.reg')
+    ff.show_regions('Bally_fig3.reg')
     ff.show_regions('edges.reg')
     ff.add_colorbar() 
     ff.colorbar.set_pad(0.5)
@@ -74,7 +74,7 @@ if NWpeak == 1:
     mincolor = 1.8
     ff.show_colorscale(cmap='gist_heat', vmin=mincolor, vmax=maxcolor, stretch='linear')
     ff.show_contour(data=nwbgps, levels=np.arange(0.4,2.0,0.2), colors='white', linewidths=0.5) 
-    ff.show_regions('rebull.reg')
+    ff.show_regions('Bally_fig3.reg')
     ff.show_regions('edges.reg')
     ff.add_colorbar() 
     ff.colorbar.set_pad(0.5)
